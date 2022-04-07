5f94b421ae11daaeb8f8409ee012ab78ad32fa01
anthonygonzales <anthony.j.gonzales22@gmail.com>
2022-04-07 14:36:54 -0500

    finished jdbc exercise

--- src/main/java/DaoFactory.java
+++ src/main/java/DaoFactory.java
@@ -1,9 +1,12 @@
+import java.sql.SQLException;
+
 public class DaoFactory {
     private static Ads adsDao;
 
     public static Ads getAdsDao() {
+        Config c = new Config();
         if (adsDao == null) {
-            adsDao = new ListAdsDao();
+            adsDao = new MySQLAdsDao(c);
         }
         return adsDao;
     }
--- src/main/java/MySQLAdsDao.java
+++ src/main/java/MySQLAdsDao.java
@@ -3,32 +3,58 @@ import com.mysql.cj.protocol.Resultset;
 
 import javax.xml.transform.Result;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MySQLAdsDao implements Ads{
 
     private Connection connection;
+//    private List<Ad> ads;
 
-    public MySQLAdsDao(Config c) throws SQLException {
-        this.connection = DriverManager.getConnection(
-                c.getUrl(),
-                c.getUser(),
-                c.getPassword()
-        );
-    }
-
-    public List<Ad> all() throws SQLException{
-        Statement statement = connection.createStatement();
-        ResultSet rs = statement.executeQuery("SELECT * FROM ads");
-        while(rs.next()){
-            System.out.println(rs.getInt("id"));
-            System.out.println(rs.getString("title");
-            System.out.println(rs.getString("description"));
+    public MySQLAdsDao(Config c) {
+//        List<Ad> ads = new ArrayList<>();
+        try {
+            DriverManager.registerDriver(new Driver());
+            this.connection = DriverManager.getConnection(
+                    c.getUrl(),
+                    c.getUser(),
+                    c.getPassword()
+            );
+        } catch (SQLException ex) {
+            ex.printStackTrace();
         }
     }
 
-    public Long insert (Ad ad){
+    public List<Ad> all() {
+        try {
+            List<Ad> ads = new ArrayList<>();
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT * FROM ads");
+            while (rs.next()) {
+                ads.add(new Ad(
+                        rs.getLong("user_id"),
+                        rs.getString("title"),
+                        rs.getString("description")
+                ));
+            }
+            return ads;
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
 
+        List<Ad> ads = null;
+        return ads;
+    }
+
+
+    public Long insert (Ad ad){
+        try{
+            Statement statement = connection.createStatement();
+            statement.executeUpdate("INSERT INTO ads (user_id, title, description) VALUES ("+ ad.getUserId() +", '"+ ad.getTitle() +"', '"+ ad.getDescription() +"')");
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        return ad.getId();
     }
 
 }
